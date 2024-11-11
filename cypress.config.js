5f11107fc4dcaf925872e3ba739b4d9a2e448b8d
Nyleve07 <Evelynv2@gmail.com>
2024-11-11 18:54:09 +1100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: true, // Enable video recording
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
